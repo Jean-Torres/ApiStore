477d9adf54cc6a6ba8d01b72e09c3f3268ff3842
Jean-Torres <carlos008torres@gmail.com>
2023-09-06 17:13:56 -0500

    Corrigiendo errores de ruta en el index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import app from './src/app';
-import './src/js/Conexion';
+import app from './app';
+import './js/Conexion';
 
 app.listen(app.get('PORT'), ()=>{
     console.log(`Servidor corriendo en el puerto: ${app.get('PORT')}`)
