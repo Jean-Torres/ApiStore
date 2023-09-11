a24f67361cde4ba880a5645a270755e628ed473a
Jean-Torres <carlos008torres@gmail.com>
2023-09-11 11:19:02 -0500

    Actualizacion de la ruta de usuarios V4

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -3,7 +3,7 @@ import usuarioModel from "../models/usuarios.model";
 export const findAllUsuario = async (req, res) => {
     try {
         const usuarios = await usuarioModel.find()
-        .populate('codigoRolXPermiso');
+            .populate('codigoRolXPermiso');
         res.json(usuarios);
     } catch (error) {
         res.status = 500;
@@ -14,25 +14,25 @@ export const findAllUsuario = async (req, res) => {
 }
 
 export const createUsuario = async (req, res) => {
-    const newUsuario = new usuarioModel({ usuario: req.body.usuario, contrasenha: req.body.contrasenha, correo: req.body.correo, codigoRolXPermiso: req.body.codigoRolXPermiso})
+    const newUsuario = new usuarioModel({ usuario: req.body.usuario, contrasenha: req.body.contrasenha, correo: req.body.correo, codigoRolXPermiso: req.body.codigoRolXPermiso })
     const usuarioSave = await newUsuario.save();
-    res.json({usuarioSave});
+    res.json({ usuarioSave });
 }
 
 export const findOneUsuario = async (req, res) => {
-    const usuario = await usuarioModel.findOne({usuario: req.query.usuario, contrasenha: req.query.contrasenha})
-    .populate('codigoRolXPermiso');
+    const usuario = await usuarioModel.findOne({ usuario: req.query.usuario, contrasenha: req.query.contrasenha })
+        .populate('codigoRolXPermiso');
     res.json(usuario)
 }
 
 export const deleteUsuario = async (req, res) => {
-    await usuarioModel.findOneAndDelete(req.query.usuario, req.query.contrasenha)
+    await usuarioModel.findOneAndDelete({ usuario: req.body.usuario, contrasenha: req.body.contrasenha })
     res.json({
         message: `${req.params.id} were deleted successfully`
     })
 }
 
 export const updateUsuario = async (req, res) => {
-    const updatedUsuario = await usuarioModel.findOneAndUpdate(req.query.usuario, req.query.contrasenha)
+    const updatedUsuario = await usuarioModel.findOneAndUpdate({ usuario: req.body.usuario, contrasenha: req.body.contrasenha }, req.body)
     res.json({ updatedUsuario })
 }
